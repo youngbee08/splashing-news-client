0efa9d5885ffe6a9cf7311335b1db4a35e2d19e7
Bamitale Abdulazeem <youngbeeh08@gmail.com>
2026-07-29 11:16:01 +0100

    refactor: clean up Google AdSense script implementation in WebLayout and update index.html

--- src/layouts/WebLayout.tsx
+++ src/layouts/WebLayout.tsx
@@ -9,27 +9,27 @@ const WebLayout = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, [location]);
 
-  useEffect(() => {
-    const scriptId = "google-adsense-script";
-    let script = document.getElementById(scriptId) as HTMLScriptElement | null;
+  // useEffect(() => {
+  //   const scriptId = "google-adsense-script";
+  //   let script = document.getElementById(scriptId) as HTMLScriptElement | null;
 
-    if (!script) {
-      script = document.createElement("script");
-      script.id = scriptId;
-      script.async = true;
-      script.src =
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7445228055826836";
-      script.crossOrigin = "anonymous";
-      document.head.appendChild(script);
-    }
+  //   if (!script) {
+  //     script = document.createElement("script");
+  //     script.id = scriptId;
+  //     script.async = true;
+  //     script.src =
+  //       "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7445228055826836";
+  //     script.crossOrigin = "anonymous";
+  //     document.head.appendChild(script);
+  //   }
 
-    return () => {
-      const existingScript = document.getElementById(scriptId);
-      if (existingScript) {
-        existingScript.remove();
-      }
-    };
-  }, []);
+  //   return () => {
+  //     const existingScript = document.getElementById(scriptId);
+  //     if (existingScript) {
+  //       existingScript.remove();
+  //     }
+  //   };
+  // }, []);
 
   return (
     <div className="min-h-screen flex flex-col bg-[#fcfcfd]">
